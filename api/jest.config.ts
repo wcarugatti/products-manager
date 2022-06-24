8ca5e78b4b95c0a90f2458be7f3e569a33a28157
Wilson Carugatti <wilson.tibia.valentin@gmail.com>
2022-06-24 17:29:49 -0300

    build: configure jest coverage

--- api/jest.config.ts
+++ api/jest.config.ts
@@ -1,6 +1,8 @@
 export default {
   clearMocks: true,
   coverageProvider: "v8",
+  collectCoverage: true,
+  collectCoverageFrom: ["src/**/*.ts", "!src/**/*.d.ts"],
   preset: "ts-jest",
   testMatch: ["**/**/*.spec.ts"],
 };
