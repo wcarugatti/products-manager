0601f79ead86f96f899696f8df835a08cb09e2ea
Wilson Carugatti <wilson.tibia.valentin@gmail.com>
2022-06-25 01:34:25 -0300

    feat: add products controller

---
+++ api/src/controllers/FileController.ts
@@ -0,0 +1,29 @@
+import AddFileToQueue from "./../useCases/AddFileToQueue";
+import { Request, Response } from "express";
+import RedisFileQueue from "./../infra/queues/RedisFileQueue";
+import AwsS3FileStorage from "../infra/gateways/AwsS3FileStorage";
+import { FileEntity } from "./../interfaces/FileEntity.d";
+
+export default class FileController {
+  static async addProducts(req: Request, res: Response) {
+    const { file } = req;
+
+    if (!file) {
+      return res.status(400).send("File not provided");
+    }
+
+    const awsS3FileStorage = AwsS3FileStorage.getInstance();
+    const redisFileQueue = RedisFileQueue.getInstance();
+    const addFileToQueue = new AddFileToQueue(awsS3FileStorage, redisFileQueue);
+
+    const fileEntity: FileEntity = {
+      file: file.buffer,
+      filename: file.originalname,
+    };
+
+    const newFilename = await addFileToQueue.execute(fileEntity);
+    res.send({
+      newFilename,
+    });
+  }
+}
