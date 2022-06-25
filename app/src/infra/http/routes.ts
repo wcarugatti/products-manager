1dd5cd286a3ec6d4a34ae9a64b7c79bd53a6bb53
Wilson Carugatti <wilson.tibia.valentin@gmail.com>
2022-06-25 02:41:36 -0300

    feat: getProductListStatus route

--- app/src/infra/http/routes.ts
+++ app/src/infra/http/routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import FileController from "./../../controllers/FileController";
-import { csvUploadMiddleware } from './../middlewares/multer';
+import { csvUploadMiddleware } from "./../middlewares/multer";
 
 const router = Router();
 
 router.post("/addProducts", csvUploadMiddleware, FileController.addProducts);
+router.get("/getProductListStatus/:filename", FileController.getProductListStatus);
 
 export { router };
