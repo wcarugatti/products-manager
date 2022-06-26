fe6b8e0edcb4709e897b5974c68f61660a40126f
Wilson Carugatti <wilson.tibia.valentin@gmail.com>
2022-06-26 04:34:14 -0300

    build: exclude migrations from jest coverage

--- app/jest.config.ts
+++ app/jest.config.ts
@@ -2,7 +2,7 @@ export default {
   clearMocks: true,
   coverageProvider: "v8",
   collectCoverage: true,
-  collectCoverageFrom: ["src/**/*.ts", "!src/**/*.d.ts"],
+  collectCoverageFrom: ["src/**/*.ts", "!src/**/*.d.ts","!**/migrations/*.ts"],
   preset: "ts-jest",
   testMatch: ["**/**/*.spec.ts"],
 };
