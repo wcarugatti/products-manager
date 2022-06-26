430ace17c642e618449868c71a039cb8cdcb6ea4
Wilson Carugatti <wilson.tibia.valentin@gmail.com>
2022-06-26 00:34:03 -0300

    build: configure database and migrations container

---
+++ app/src/infra/postgres/config/ormconfig.ts
@@ -0,0 +1,13 @@
+import "dotenv/config";
+import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions";
+
+export const ormConfig: PostgresConnectionOptions = {
+  type: "postgres",
+  host: process.env.POSTGRES_HOST,
+  port: +process.env.POSTGRES_PORT,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  entities: ["src/infra/postgres/models/**/*.ts"],
+  migrations: ["src/infra/postgres/migrations/**/*.ts"],
+};
