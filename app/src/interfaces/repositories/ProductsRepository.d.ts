5958e66c26e5750a1a6f5c6b58e6332bfbece2b4
Wilson Carugatti <wilson.tibia.valentin@gmail.com>
2022-06-26 18:49:15 -0300

    feat: update repository interface

--- app/src/interfaces/repositories/ProductsRepository.d.ts
+++ app/src/interfaces/repositories/ProductsRepository.d.ts
@@ -2,4 +2,11 @@ import { ProductEntity } from "../entities/ProductEntity";
 
 export interface ProductsRepository {
   addProducts(productsList: ProductEntity[]): Promise<void>;
+  removeProduct(productId: string): Promise<void>;
+  getProducts(): Promise<ProductModel[]>;
+  getProduct(productId: string): Promise<ProductModel>;
+  updateProduct(
+    productId: string,
+    newProduct: Partial<ProductEntity>,
+  ): Promise<void>;
 }
