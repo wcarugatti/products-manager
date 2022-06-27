26270cd99947f7f8978fcc61bb2d2b7f0d459115
Wilson Carugatti <wilson.tibia.valentin@gmail.com>
2022-06-26 23:09:55 -0300

    test: complete integration tests

--- app/tests/integration/ProductList.spec.ts
+++ app/tests/integration/ProductList.spec.ts
@@ -9,10 +9,11 @@ describe("Product List integration test", () => {
     app = await createExpressApp();
   });
   let jobId: string;
+  let currentProductId: string;
 
   it("should add csv file to the queue and to storage", async () => {
     const res = await request(app)
-      .post("/addProductList")
+      .post("/product-list")
       .attach("csvFile", path.resolve(__dirname, "..", "./mocks/products.csv"))
       .expect(200);
 
@@ -20,15 +21,55 @@ describe("Product List integration test", () => {
   });
 
   it("should return file status", async () => {
-    await request(app)
-      .get("/getProductListStatus/" + jobId)
+    await new Promise((delay) => setTimeout(delay, 2000));
+    const statusResponse = await request(app)
+      .get("/product-list-status/" + jobId)
       .expect(200);
+    expect(statusResponse.body.productListStatus).toBe("completed");
   });
-  
+
   it("should return status code 404", async () => {
-    const randomString = "random string"
+    const randomString = "random string";
     await request(app)
-      .get("/getProductListStatus/" + randomString)
+      .get("/product-list-status/" + randomString)
+      .expect(404);
+  });
+
+  it("should return the added products", async () => {
+    const res = await request(app).get("/products").expect(200);
+    expect(res.body).toHaveLength(6);
+
+    currentProductId = res.body[3].id;
+  });
+
+  it("should update product and return it updated", async () => {
+    const updateResponse = await request(app)
+      .patch("/product/" + currentProductId)
+      .send({
+        name: "test_name",
+      });
+
+    const getResponse = await request(app)
+      .get("/product/" + currentProductId)
+      .expect(200);
+    expect(getResponse.body.name).toBe("test_name");
+  });
+
+  it("should remove product from the database", async () => {
+    await request(app)
+      .delete("/product/" + currentProductId)
+      .expect(200);
+    await request(app)
+      .get("/product/" + currentProductId)
+      .expect(404);
+    await request(app)
+      .delete("/product/" + currentProductId)
+      .expect(404);
+    await request(app)
+      .patch("/product/" + currentProductId)
+      .send({
+        name: "test_name",
+      })
       .expect(404);
   });
 });
